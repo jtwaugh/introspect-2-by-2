ddbc0b3481bdfe7c443b2b1b604298c322b2a0e3
jtwaugh <james.t.waugh@gmail.com>
2024-08-27 20:32:29 -0400

    Add splash screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,12 @@ import { updateValueAtIndex } from "@/lib/utils";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const questions = ["What's on your mind?", "What part of this experience causes suffering?", "Describe the suffering as succinctly as possible. Let's give it a name."];
+  const questions = ["Think about a past experience you're blaming yourself for. What comes to mind?", "What part of this experience causes suffering? Especially salient is suffering that feels like it must be your own fault.", "Describe the suffering as succinctly as possible. Give it a name, or try to be as specific as possible about what component of the memory is causing you to ache."];
 
+  const [isSplashPageComplete, setIsSplashPageComplete] = useState(false);
   const [isQuestionnaireComplete, setIsQuestionnaireComplete] = useState(false);
   const [isTwoByTwoComplete, setIsTwoByTwoComplete] = useState(false);
   const [questionnaireAnswers, setQuestionnaireAnswers] = useState<string[]>(questions);
@@ -25,6 +27,7 @@ export default function Home() {
       </div>
       <div className="min-h-screen flex items-center justify-center">
         {
+          isSplashPageComplete ? (
           isQuestionnaireComplete ? (
             isTwoByTwoComplete ? 
               <AnswersReadout description={questionnaireAnswers[questionnaireAnswers.length - 1]} answers={twoByTwoResponses}/> : 
@@ -48,8 +51,13 @@ export default function Home() {
                     }
                   />
                 </div>
+            </div>) :
+            <div className="h-full flex flex-col justify-center">
+              <div className='rounded-md border border-input p-4 text-center'>
+              <span className='title pb-4 text-center'>{"This is an introspection tool focused on "} <i>blame.</i><br/> {"When we think about past suffering, we often tend to blame ourselves."}<br/><br/>{"This blame can rely on a very "} <i>static conception</i>{" of the self."}<br/><br/>{"With this tool, you will examine some self-blame resulting from a past experience."} <br/> {"You will try to deconstruct this static conception by splitting the experience into several causes."}<br/><br/>{"This practice can help provide a new and freeing perspective on your experience!"}</span>
             </div>
-            
+            <Button onClick={() => setIsSplashPageComplete(true)}>Start</Button>
+            </div>  
           }
           
       </div>
